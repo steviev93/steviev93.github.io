ec8ea239932a9a295030f90c43092b12296df986
Stephen Valente <stephen.valente12@gmail.com>
2022-05-10 11:45:51 -0400

    Second Commit

--- src/app/certifications/certifications.component.ts
+++ src/app/certifications/certifications.component.ts
@@ -6,10 +6,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./certifications.component.css']
 })
 export class CertificationsComponent implements OnInit {
-
-  constructor() { }
+  certifications: Array<{title: string, exDate: string}>;;
+  constructor() {
+    this.certifications = [{title:"AWS Certified Cloud Practitioner", exDate: "January 2022 - 2025"},
+                      {title:"Microsoft Certified: Azure Fundamentals", exDate: "February 2022 - "},
+                      {title:"Coursera: Deep Learning Specialization", exDate: "June 2020 - "}];
+   }
 
   ngOnInit(): void {
+    
   }
 
 }
